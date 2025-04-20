0852c9e34ad2a348a2b2085ca07b4a3fca8eae10
Arthur L Molina <arthur.molina.dev@gmail.com>
2025-04-20 13:04:39 -0300

    Update Cadastro De Aluno (Alocação dinâmica de memória).c

--- Cadastro De Aluno (Alocação dinâmica de memória).c
+++ Cadastro De Aluno (Alocação dinâmica de memória).c
@@ -16,8 +16,17 @@ int main(){
 	printf("Quantos alunos voce deseja cadastrar? ");
 	scanf("%d", &n);
 	getchar();
-	float media[n];
+	float *media;
+	media = malloc(n*sizeof(float));
+	if (media==NULL){
+		printf("Erro ao alocar a memoria da media");
+		return 1;
+	}
 	ptr = malloc(n*sizeof(struct Aluno));
+	if(ptr==NULL){
+		printf("Erro ao alocar memória das structs");
+		return 1;
+	}
 	for(i=0; i<n; i++){
 		printf("Cadastro aluno %d \n", i+1);
 		printf("Nome: ");
@@ -31,11 +40,19 @@ int main(){
 			while(( ch = getchar()) != '\n' && ch != EOF );
 		}
 		ptr[i].nome = malloc((tam+1)*sizeof(char));
+		if(ptr[i].nome == NULL ){
+			printf("Erro ao alocar a memoria do aluno %d", i+1);
+			return 1;
+		}
 		strcpy(ptr[i].nome, nomeTmp);
 		
 		printf("Quantas notas deseja armazenar? ");
 		scanf("%d", &ptr[i].qtdNotas);
 		ptr[i].notas = malloc(ptr[i].qtdNotas*sizeof(float));
+		if(ptr[i].notas==NULL){
+			printf("Erro ao alocar a memoria da(s) nota(s) do aluno %d", i+1);
+			return 1;
+		}
 		media[i]=0;
 		for (i2=0; i2<ptr[i].qtdNotas; i2++){
 			printf("Nota %d: ", i2+1);
@@ -53,10 +70,19 @@ int main(){
 		printf("--Notas--\n");
 		for (i2=0; i2<ptr[i].qtdNotas; i2++){
 			printf("Nota %d: %.2f\n", i2+1, ptr[i].notas[i2]);
+			
 		}
 		printf("Media das notas: %.2f\n", media[i]);
 		
 		printf("----------\n");
 	}
+	//liberando blocos alocados
+	for (i=0; i<n; i++){
+		free(ptr[i].nome);
+		free(ptr[i].notas);
+	}
+	free(media);
+	free(ptr);
+	
 	return 0;
 }
